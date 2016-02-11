547875dfa563632fda67466d5f4cb582c959ba6f
Amaru Cuba Gyllensten <amarucuba@gmail.com>
2016-02-11 13:45:42 +0100

    cleaned and added size

--- src/Data/Bihash.hs
+++ src/Data/Bihash.hs
@@ -1,34 +1,50 @@
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE BangPatterns #-}
 
-module Data.Bihash 
-    (op, fw, bw, fromList, singleton, filter, toList
-    ) where
+module Data.Bihash
+       ( op
+       , fw
+       , bw
+       , empty
+       , singleton
+       , toList
+       , fromList
+       , insert
+       , size
+       , mapMaybe
+       , filter
+       ) where
 
-import Prelude hiding (filter , lookup, (.),id) 
-import Control.Category 
+import Prelude hiding (filter , lookup, (.),id)
+import Control.Category
 import Data.HashMap.Strict (HashMap)
-import qualified Data.HashMap.Strict as HM 
+import qualified Data.HashMap.Strict as HM
 import Data.Hashable
-import Data.Maybe hiding (mapMaybe) 
+import Data.Maybe hiding (mapMaybe)
 
 import Data.Semigroupoid
 import Data.Bifunctor
 
 data Bihash a b where
-  Bihash :: (Eq a, Hashable a, Eq b, Hashable b) => !(HashMap a b) -> !(HashMap b a) -> Bihash a b
+  Bihash :: (Eq a, Hashable a, Eq b, Hashable b) => {-# UNPACK #-} !(HashMap a b) -> {-# UNPACK #-}!(HashMap b a) -> Bihash a b
 
-singleton :: (Eq a, Hashable a, Eq b, Hashable b) => a -> b -> Bihash a b 
+empty :: (Eq a, Hashable a, Eq b, Hashable b) => Bihash a b
+empty = Bihash HM.empty HM.empty
+
+singleton :: (Eq a, Hashable a, Eq b, Hashable b) => a -> b -> Bihash a b
 singleton a b = Bihash (HM.singleton a b) (HM.singleton b a)
 
 fromList :: (Eq a, Hashable a, Eq b, Hashable b) => [(a,b)] -> Maybe (Bihash a b)
 fromList xs = let assertInj :: (Eq a, Hashable a) => [(a,b)] -> Maybe (HashMap a b)
-                  assertInj = sequenceA . HM.fromListWith (\_ _ -> Nothing) . fmap (second Just) 
-                  flip (a,b) = (b,a)
-              in Bihash <$> assertInj xs <*> assertInj (fmap flip xs)
+                  assertInj = sequenceA . HM.fromListWith (\_ _ -> Nothing) . fmap (second Just)
+                  swap (a,b) = (b,a)
+              in Bihash <$> assertInj xs <*> assertInj (fmap swap xs)
 
-insert :: (Eq a, Hashable a, Eq b, Hashable b)=> a -> b -> Bihash a b -> Maybe (Bihash a b)
-insert a b (Bihash ab ba) = if HM.member a ab || HM.member b ba then Nothing else Just $ Bihash (HM.insert a b ab) (HM.insert b a ba)   
+insert :: (Eq a, Hashable a, Eq b, Hashable b) => a -> b -> Bihash a b -> Maybe (Bihash a b)
+insert a b (Bihash ab ba) = if HM.member a ab || HM.member b ba then Nothing else Just $ Bihash (HM.insert a b ab) (HM.insert b a ba)
+
+size :: Bihash a b -> Int
+size (Bihash ab _) = HM.size ab
 
 toList :: (Eq a, Hashable a, Eq b, Hashable b) => Bihash a b -> [(a,b)]
 toList (Bihash ab _) = HM.toList ab
@@ -47,8 +63,8 @@ filter p (Bihash ab ba) = Bihash (HM.filterWithKey p ab) (HM.filterWithKey (flip
 
 instance Semigroupoid Bihash where
   (Bihash bc cb) `o` (Bihash ab ba) = let ba' = ($ba) . HM.lookup
-                                          bc' = ($bc) . HM.lookup 
-                                      in Bihash (mapMaybe bc' ab) (mapMaybe ba' cb) 
+                                          bc' = ($bc) . HM.lookup
+                                      in Bihash (mapMaybe bc' ab) (mapMaybe ba' cb)
 
 mapMaybe :: (a -> Maybe b) -> HashMap k a -> HashMap k b
 mapMaybe f = fmap fromJust . HM.filter isJust . fmap f
